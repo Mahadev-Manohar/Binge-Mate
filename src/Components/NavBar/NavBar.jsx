2e9c34665dd349531aefb53108a4f00a422a1a83
Mahadev-Manohar <mahadevmanohar07@gmail.com>
2025-03-19 05:58:36 +0530

    Netflix-clone => Binge Mate

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,10 +4,13 @@ import "./NavBar.css"
 function NavBar() {
   return (
     <div className='navbar'>
-      <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix-logo" />
-      <img className='avatar' src="./Netflix-avatar.png" alt="Avatar" />
+      <img className='logo' src="./B.png" alt="Binge-logo" />
+      <img className='avatar' src="./Avatar.png" alt="Avatar" />
     </div>
   )
 }
 
 export default NavBar
+
+//https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png
+//netflix logo
